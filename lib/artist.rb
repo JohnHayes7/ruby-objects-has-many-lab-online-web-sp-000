2e1630f23204fdaff49bb86e44d5ed7c37a25071
John Hayes <johnnyhayes7@gmail.com>
2019-02-07 14:07:25 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,14 +14,14 @@ class Artist
   
   def add_song(song)
     @songs_array << song
-    #song.artist = self
+    song.artist = self
     binding.pry
   end
   
   def add_song_by_name(song)
-    #new_song = Song.new(new_song)
-    #@songs_array << song
-    #song.artist = self
+    new_song = Song.new(new_song)
+    @songs_array << song
+    song.artist = self
   end
   
   def self.song_count
